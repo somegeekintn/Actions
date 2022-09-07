d02ab6be4ff189a7e390298336e529f06ea3459f
Casey Fleser <cfleser@gmail.com>
2022-09-07 09:15:47 -0500

    Initial Commit

---
+++ Actions/ActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionsApp.swift
+//  Actions
+//
+//  Created by Casey Fleser on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct ActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
